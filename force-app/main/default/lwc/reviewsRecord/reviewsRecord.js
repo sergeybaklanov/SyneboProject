e8f0196bfc9b02a0bb88f71475387c577ea25464
SerhiiB <sergeybaklanov468@gmail.com>
2023-04-03 17:51:27 +0200

    Start creating tests for reviewsRecord

--- force-app/main/default/lwc/reviewsRecord/reviewsRecord.js
+++ force-app/main/default/lwc/reviewsRecord/reviewsRecord.js
@@ -1,13 +1,15 @@
 import { LightningElement, api } from 'lwc';
 import pullReviews from '@salesforce/apex/ReviewsController.pullReviews';
-import movieRecordId from '@salesforce/apex/ReviewsController.movieRecordId';
 
 export default class ReviewsRecord extends LightningElement {
     @api recordId;
 
-    @api invoke() {
+    @api async invoke() {
         console.log("Hi, I'm an action.");
-        movieRecordId=this.recordId;
-        pullReviews({movieRecordId: this.recordId}).then().catch(err => console.log(err));
-      }
+            try{
+        const result = await pullReviews({movieRecordId: this.recordId});
+        } catch(error){
+          console.log(error);
+      }   
+  }
 }
