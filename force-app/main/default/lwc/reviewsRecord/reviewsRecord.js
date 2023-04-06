284d56d346e5f17b5486a19bff5da5cfa7ee0c4e
SerhiiB <sergeybaklanov468@gmail.com>
2023-04-06 08:54:01 +0200

    Start working on task 6

--- force-app/main/default/lwc/reviewsRecord/reviewsRecord.js
+++ force-app/main/default/lwc/reviewsRecord/reviewsRecord.js
@@ -1,15 +1,48 @@
 import { LightningElement, api } from 'lwc';
+import {ShowToastEvent} from 'lightning/platformShowToastEvent';
 import pullReviews from '@salesforce/apex/ReviewsController.pullReviews';
 
 export default class ReviewsRecord extends LightningElement {
     @api recordId;
+     isButtonDisable = false;
 
     @api async invoke() {
-        console.log("Hi, I'm an action.");
-            try{
+        if(!this.isButtonDisable){
+            this.isButtonDisable=true;
+            console.log("Hi, I'm an action.");
+        try{
         const result = await pullReviews({movieRecordId: this.recordId});
+          
+        this.showSuccessMessage();
         } catch(error){
           console.log(error);
-      }   
+          this.showErrorMessage(error); } 
+      setTimeout(()=>{ this.isButtonDisable =false;},10000);
+       
+         } else{
+            console.log('Button disable');
+        return;  
+    }
+    
   }
+
+showSuccessMessage() {
+    const evt = new ShowToastEvent({
+        title: 'Success',
+        message: 'Reviews records were updated.',
+        variant: 'success',
+    });
+    this.dispatchEvent(evt);
 }
+
+showErrorMessage(error) {
+    const evt = new ShowToastEvent({
+        title: 'Error',
+        message: 'An error has occurred:' + error,
+        variant: 'error',
+    });
+    this.dispatchEvent(evt);
+}
+
+}
+
